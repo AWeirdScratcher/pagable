ecc3a40a3f03c25f1ebe38be93cc1e7a66b15cef
AWeirdScratcher <aweirdscratcher@gmail.com>
2023-10-22 09:49:56 +0000

    [doc] added doc

--- pagable/utils.py
+++ pagable/utils.py
@@ -4,10 +4,25 @@ from typing import Any, Callable, Dict, List
 
 requirements: List[str] = []
 
-def clear_requirements(fn: str):
+def clear_requirements():
+    """(interal) clear all requirements."""
     requirements.clear()
 
 def require(file: str):
-    dest = os.path.join(os.getcwd(), "scripts", file)
+    """Requires a JS or CSS file from the ``src/scripts`` or ``src/styles`` folder.
+    
+    Args:
+        file (str): The file.
+    """
+    if not file.endswith('js') and not file.endswith('css'):
+        raise NotImplementedError(
+            f"{file.split('.')[-1]!r} is not yet implemented."
+        )
+
+    dest = os.path.join(
+        os.getcwd(), 
+        "scripts" if file.endswith('js') else "css", 
+        file
+    )
 
     requirements.append(dest)
